ee770aa78d9be417866913b27983341cab55bb7f
meric erkosun <meric.erkosun@gmail.com>
2024-03-13 16:10:03 +0300

    Basic Quiz App. deleted.

--- Basic Quiz App./android/app/src/main/kotlin/com/example/quiz_uygulamasi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.quiz_uygulamasi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Basic Quiz App./lib/Bayraklar.dart
+++
@@ -1,8 +0,0 @@
-class Bayraklar {
-
-  int bayrak_id;
-  String bayrak_ad;
-  String bayrak_resim;
-
-  Bayraklar(this.bayrak_id, this.bayrak_ad, this.bayrak_resim);
-}
