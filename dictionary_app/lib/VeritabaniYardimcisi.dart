76e3bec42b46c5aadb5e4b443390f91f35f444d4
meric erkosun <meric.erkosun@gmail.com>
2024-03-20 13:07:22 +0300

    Database bug fixed.

--- dictionary_app/lib/VeritabaniYardimcisi.dart
+++ dictionary_app/lib/VeritabaniYardimcisi.dart
@@ -11,11 +11,11 @@ class VeritabaniYardimcisi{
   static Future<Database> veritabaniErisim() async {
     String veritabaniYolu = join(await getDatabasesPath(), veritabaniAdi);
 
-    if(await databaseExists(veritabaniYolu)){//Veritabanı var mı yok mu kontrolü
+    if(await databaseExists(veritabaniYolu)){ //Veritabanı var mı yok mu kontrolü
       print("Veri tabanı zaten var.Kopyalamaya gerek yok");
     }else{
       //assetten veritabanının alınması
-      ByteData data = await rootBundle.load("veritabani/$veritabaniAdi");
+      ByteData data = await rootBundle.load("database/$veritabaniAdi");
       //Veritabanının kopyalama için byte dönüşümü
       List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
       //Veritabanının kopyalanması.
